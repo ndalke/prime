5f4b3fb2852b9e7ea69093b7e99a5b9249ce4255
ndalke <97801641+ndalke@users.noreply.github.com>
2022-01-17 23:01:16 +0530

    Add files via upload

---
+++ DuplicateElementArray/DuplicateElementArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Dublicate
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter number of Element in an array");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        Console.WriteLine("Enter " + n + " Elements...");
+        for (int i = 0; i < n; i++)
+        {
+
+            arr[i] =int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Duplicate elements:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[i] == arr[j])
+                    Console.WriteLine(arr[i]+ " ");
+            }
+        }
+    }
+}
