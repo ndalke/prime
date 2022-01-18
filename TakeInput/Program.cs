f3edad7c35a696b587bba279a15254695ccf16e3
ndalke <97801641+ndalke@users.noreply.github.com>
2022-01-18 14:20:07 +0530

    Add files via upload

---
+++ TakeInput/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+public class MultiArrayExample
+{
+    public static void Main(string[] args)
+    {
+        //declaration of 2D array  
+        int[,] arr3 = new int[3,3];
+        int[,] arr4 = new int[3, 3];
+
+       //store elements in first matrix
+        Console.WriteLine("Enter elements of first Matrix");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                int data = int.Parse(Console.ReadLine());
+                arr3[i, j] = data;
+            }
+        }
+
+        //store elements in secound matrix
+        Console.WriteLine("Enter elements of Second Matrix");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                int data = int.Parse(Console.ReadLine());
+                arr4[i, j] = data;
+            }
+        }
+
+        //display first matrix
+        Console.WriteLine("First Matrix");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(arr3[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+        //display second matrix
+        Console.WriteLine("Second Matrix");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                
+                Console.Write(arr4[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        //addition of two matrix
+        Console.WriteLine("Addition");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+              
+                Console.Write((arr3[i, j] + arr4[i, j]) + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
